1d0956001b2bb6d852f03cb2d1a41214ef69c1cb
Miguel Dias <mdias1300@gmail.com>
2025-06-24 20:46:03 -0300

    programa feito

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        Console.WriteLine("-ANEL DE CORAÇÃO-");
+        Console.ResetColor();
+        Console.Write("");
+        Thread.Sleep(2000);
+        Console.Write("Quantos elementos da sequência deseja exibir? ");
+
+        if (int.TryParse(Console.ReadLine(), out int quantidade) && quantidade > 0)
+        {
+            ExibirSequenciaTriangular(quantidade);
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Entrada inválida! Por favor, digite um número inteiro positivo (Ex. 1,2,3...).");
+            Console.ResetColor();
+        }
+    }
+    static void ExibirSequenciaTriangular(int n)
+    {
+        int eventos = 0;
+        int dificuldade = 1;
+
+        Console.ForegroundColor = ConsoleColor.DarkGreen;
+        Thread.Sleep(1500);
+        Console.WriteLine($"\n{n} primeiros elementos da sequência:");
+        Console.ResetColor();
+        for (int i = 0; i < n; i++)
+        {
+
+            Thread.Sleep(500);
+            eventos += dificuldade;
+            Console.Write(eventos + (i < n - 1 ? " " : "\n"));
+            dificuldade++;
+        }
+    }
+}
